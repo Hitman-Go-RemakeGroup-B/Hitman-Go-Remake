9c16d20eed9435ae1111b27d95ff220514a85090
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-12-19 18:13:27 +0100

    menu panel

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] int sceneIndex;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
 }
