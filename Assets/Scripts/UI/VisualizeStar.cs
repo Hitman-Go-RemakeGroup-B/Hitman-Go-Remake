ffd8710b4d81723b3187d4011277288dd24beae7
Alberto Tarabella <alberto.tarabella04@gmail.com>
2025-01-21 14:29:34 +0100

    level selector ui

---
+++ Assets/Scripts/UI/VisualizeStar.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows;
+
+public class VisualizeStar : MonoBehaviour
+{
+    [SerializeField]SaveDataJson saveDataJson;
+    [SerializeField] Image[] Star;
+    void Start()
+    {
+        saveDataJson.VisualizeStar(GetIndex());
+        for (int i = 0;i<saveDataJson.starCount;i++)
+        {
+            var tempColor = Star[i].color;
+            tempColor.a = 1f;
+            Star[i].color = tempColor;
+        }
+    }
+
+    public int GetIndex()
+    {
+        return int.Parse(this.name.Split(' ')[0]);
+    }
+}
